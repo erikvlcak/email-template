1fa124c68dde04b9aeeef3284f5b5853a1f2df42
erikvlcak <erikvlcak1@gmail.com>
2024-11-16 15:06:04 +0100

    add route to index view

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MailController;
 
+Route::get('/', function () {
+    return view('index');
+});
+
 Route::post('/send-email', [MailController::class, 'sendEmail'])->name('send.email');
 Route::get('/api/emails', [MailController::class, 'getEmails']);
