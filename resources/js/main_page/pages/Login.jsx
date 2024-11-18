ff38fd24e47332f2041c5dc6d4ce55804ea72760
Zerokoska <vasulhavruliv1.c@gmail.com>
2024-11-18 11:14:40 +0100

    some minor changes

--- resources/js/main_page/pages/Login.jsx
+++
@@ -1,9 +0,0 @@
-function Login(){
-    return(
-        <div>
-            <h1>something Login</h1>
-        </div>
-    )
-}
-
-export default Login;
--- resources/js/pages/Register.jsx
+++ resources/js/pages/Register.jsx
@@ -1,3 +1,69 @@
-export default function Register() {
-    return <div>Register</div>;
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+ 
+export default function Register(props) {
+ 
+    const [values, setValues] = useState({
+        email: '',
+        name: '',
+        password: '',
+        password_confirmation: ''
+    })
+ 
+    const handleSubmit = async (event) => {
+ 
+        event.preventDefault();
+ 
+        // // with axios
+        try {
+            // make the AJAX request
+            const response = await axios.post('/register', values);
+            // get the (already JSON-parsed) response data
+            const response_data = response.data;
+        } catch (error) {
+            // if the response code is not 2xx (success)
+            switch (error.response.status) {
+                case 422:
+                    // handle validation errors here
+                    console.log('VALIDATION FAILED:', error.response.data.errors);
+                    break;
+                case 500:
+                    console.log('UNKNOWN ERROR', error.response.data);
+                    break;
+            }
+        }
+    }
+ 
+    const handleChange = (event) => {
+        setValues(previous_values => {
+            return ({...previous_values,
+                [event.target.name]: event.target.value
+            });
+        });
+    }
+ 
+    return (
+        <form action="/register" method="post" onSubmit={ handleSubmit }>
+            <label htmlFor="name">Name: </label>
+ 
+            <input type="text" name="name" value={ values.name } onChange={ handleChange } />
+ 
+            <label htmlFor="email">Email: </label>
+
+            <input type="email" name="email" value={ values.email } onChange={ handleChange } />
+            
+            <label htmlFor="password">Password: </label>
+
+            <input type="password" name="password" value={ values.password } onChange={ handleChange } />
+
+            <label htmlFor="password_confirmation">Password confirmation: </label>
+
+            <input type="password" name="password_confirmation" value={ values.password_confirmation } onChange={ handleChange } />
+
+            <label htmlFor="password"></label>
+
+            <button>Register</button>
+ 
+        </form>
+    );
 }
